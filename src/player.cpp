8b41be8ede1b2571b8b4e6e888de57955d471ddd
arm-cast <armando.castillo@mavs.uta.edu>
2020-03-31 00:41:35 -0500

    player/spectator updates

--- src/player.cpp
+++ src/player.cpp
@@ -1,13 +1,16 @@
 #include "player.hpp"
 
-Player::Player()
-{
-
-}
+Player::Player(Hand playerHand, std::string name, std::string playerID)
+    : playerHand{playerHand}, name{name}, playerID{playerID} { }
 
 Player::~Player() { }
 
 virtual void deliver(const chat_message msg)
 {
-  
+  bool write_in_progress = !write_msgs_.empty();
+    write_msgs_.push_back(msg);
+    if (!write_in_progress)
+    {
+      do_write();
+    }
 }
