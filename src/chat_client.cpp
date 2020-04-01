c55e875e3e4a4f02154e6ba8e73044cb9af9336f
Duke-Ly <duke.ly@mavs.uta.edu>
2020-04-01 00:37:43 -0500

    integrated gui into chat_client

--- src/chat_client.cpp
+++ src/chat_client.cpp
@@ -1,20 +1,4 @@
-/*
- * CSE3310-003 Spring 2020
- * Project Prototyping
- * Duke Ly
- * 1001296968
- * 02/04/2020
-*/
-//
-// chat_client.cpp
-// ~~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2018 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
+#include <gtkmm.h>
 #include <cstdlib>
 #include <deque>
 #include <iostream>
@@ -26,6 +10,21 @@
 
 using asio::ip::tcp;
 using json = nlohmann::json;
+
+Gtk::Window* window = nullptr;
+Gtk::Button* button_check = nullptr;
+Gtk::Button* button_call = nullptr;
+Gtk::Button* button_fold = nullptr;
+Gtk::Button* button_bet = nullptr;
+Gtk::Button* button_raise = nullptr;
+Gtk::Button* button_all_in = nullptr;
+Gtk::Button* button_replace = nullptr;
+Gtk::CheckButton* check1 = nullptr;
+Gtk::CheckButton* check2 = nullptr;
+Gtk::CheckButton* check3 = nullptr;
+Gtk::CheckButton* check4 = nullptr;
+Gtk::CheckButton* check5 = nullptr;
+Gtk::Entry* player_bet_entry = nullptr;
 
 typedef std::deque<chat_message> chat_message_queue;
 
@@ -145,57 +144,223 @@ private:
     chat_message_queue write_msgs_;
 };
 
+static void player_bet_entry_activate()
+{
+    if (window)
+    {
+        if (player_bet_entry)
+        {
+            std::cout << "$" + player_bet_entry->get_text() + " bet was set.\n\n";
+        }
+    }
+}
+
+static void check1_toggled()
+{
+    if (window)
+    {
+        if (check1)
+            std::cout << "Check Box 1 checked.\n\n";
+    }
+}
+
+static void check2_toggled()
+{
+    if (window)
+    {
+        if (check2)
+            std::cout << "Check Box 2 checked.\n\n";
+    }
+}
+
+static void check3_toggled()
+{
+    if (window)
+    {
+        if (check3)
+            std::cout << "Check Box 3 checked.\n\n";
+    }
+}
+
+static void check4_toggled()
+{
+    if (window)
+    {
+        if (check4)
+            std::cout << "Check Box 4 checked.\n\n";
+    }
+}
+
+static void check5_toggled()
+{
+    if (window)
+    {
+        if (check5)
+            std::cout << "Check Box 5 checked.\n\n";
+    }
+}
+
+static void on_check_button_clicked()
+{
+    if(window)
+    {
+        if (button_check)
+            std::cout << "Check Button was clicked.\n\n";
+    }
+}
+
+static void on_call_button_clicked()
+{
+    if (window)
+    {
+        if (button_call)
+            std::cout << "Call Button was clicked.\n\n";
+    }
+}
+
+static void on_fold_button_clicked()
+{
+    if (window)
+    {
+        if (button_fold)
+            std::cout << "Fold Button was clicked.\n\n";
+    }
+}
+
+static void on_bet_button_clicked()
+{
+    if (window)
+    {
+        if (button_bet)
+            std::cout << "Bet Button was clicked.\n\n";
+    }
+}
+
+static void on_raise_button_clicked()
+{
+    if (window)
+    {
+        if (button_raise)
+            std::cout << "Raise Button was clicked.\n\n";
+    }
+}
+
+static void on_all_in_button_clicked()
+{
+    if (window)
+    {
+        if (button_all_in)
+            std::cout << "All-in Button was clicked.\n\n";
+    }
+}
+
+static void on_replace_button_clicked()
+{
+    if (window)
+    {
+        if (button_replace)
+            std::cout << "Replace Button was clicked.\n\n";
+    }
+}
+
 int main(int argc, char* argv[])
 {
-    try
+    int gc = 1;
+    auto app = Gtk::Application::create(gc, argv, "Poker++");
+    auto refBuilder = Gtk::Builder::create();
+    refBuilder->add_from_file("../assets/gui_mockup.glade");
+    refBuilder->get_widget("window", window);
+
+    if (window)
     {
-        if (argc != 3)
+        refBuilder->get_widget("button_check", button_check);
+        refBuilder->get_widget("button_call", button_call);
+        refBuilder->get_widget("button_fold", button_fold);
+        refBuilder->get_widget("button_bet", button_bet);
+        refBuilder->get_widget("button_raise", button_raise);
+        refBuilder->get_widget("button_all_in", button_all_in);
+        refBuilder->get_widget("button_replace", button_replace);
+        refBuilder->get_widget("check1", check1);
+        refBuilder->get_widget("check2", check2);
+        refBuilder->get_widget("check3", check3);
+        refBuilder->get_widget("check4", check4);
+        refBuilder->get_widget("check5", check5);
+        refBuilder->get_widget("player_bet_entry", player_bet_entry);
+
+        if (button_check)
         {
-            std::cerr << "Usage: chat_client <host> <port>\n";
-            return 1;
+            button_check->signal_clicked().connect(sigc::ptr_fun(on_check_button_clicked));
         }
-
-        asio::io_context io_context;
-
-        tcp::resolver resolver(io_context);
-        auto endpoints = resolver.resolve(argv[1], argv[2]);
-        chat_client c(io_context, endpoints);
-
-        std::thread t([&io_context]()
+        if (button_call)
         {
-            io_context.run();
-        });
-
-        char line[chat_message::max_body_length + 1];
-        while (std::cin.getline(line, chat_message::max_body_length + 1))
-        {   //CSE3310 This is where program accepts input from the user via cin
-            json j;
-            j["name"] = "duke";
-            j["id"] = "1001296968";
-            time_t tt;
-            struct tm * ti;
-            time (&tt);
-            ti = localtime(&tt);
-            std::string str = asctime(ti);
-            str[str.length()-1] = ' ';
-            j["chrono"] = str;
-            std::string s = j.dump();
-            s.append(line);
-            strcpy(line, s.c_str());
-            chat_message msg;
-            msg.body_length(std::strlen(line));
-            std::memcpy(msg.body(), line, msg.body_length());
-            msg.encode_header();
-            c.write(msg); // CSE3310 This is where the program send the string msg to be processed and sent to server
+            button_call->signal_clicked().connect(sigc::ptr_fun(on_call_button_clicked));
         }
-
-        c.close();
-        t.join();
+        if (button_fold)
+        {
+            button_fold->signal_clicked().connect(sigc::ptr_fun(on_fold_button_clicked));
+        }
+        if (button_bet)
+        {
+            button_bet->signal_clicked().connect(sigc::ptr_fun(on_bet_button_clicked));
+        }
+        if (button_raise)
+        {
+            button_raise->signal_clicked().connect(sigc::ptr_fun(on_raise_button_clicked));
+        }
+        if (button_all_in)
+        {
+            button_all_in->signal_clicked().connect(sigc::ptr_fun(on_all_in_button_clicked));
+        }
+        if (button_replace)
+        {
+            button_replace->signal_clicked().connect(sigc::ptr_fun(on_replace_button_clicked));
+        }
+        if (check1)
+        {
+            check1->signal_clicked().connect(sigc::ptr_fun(check1_toggled));
+        }
+        if (check2)
+        {
+            check2->signal_clicked().connect(sigc::ptr_fun(check2_toggled));
+        }
+        if (check3)
+        {
+            check3->signal_clicked().connect(sigc::ptr_fun(check3_toggled));
+        }
+        if (check4)
+        {
+            check4->signal_clicked().connect(sigc::ptr_fun(check4_toggled));
+        }
+        if (check5)
+        {
+            check5->signal_clicked().connect(sigc::ptr_fun(check5_toggled));
+        }
+        if (player_bet_entry)
+        {
+            player_bet_entry->signal_activate().connect(sigc::ptr_fun(player_bet_entry_activate));
+        }
     }
-    catch (std::exception& e)
+
+    if (argc != 3)
     {
-        std::cerr << "Exception: " << e.what() << "\n";
+        std::cerr << "Usage: chat_client <host> <port>\n";
+        return 1;
     }
 
+    asio::io_context io_context;
+
+    tcp::resolver resolver(io_context);
+    auto endpoints = resolver.resolve(argv[1], argv[2]);
+    chat_client c(io_context, endpoints);
+
+    std::thread t([&io_context]()
+    {
+        io_context.run();
+    });
+    app->run(*window);
+    c.close();
+    t.join();
+
+    delete window;
     return 0;
 }
